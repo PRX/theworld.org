5eb62680944c46aac4daec3e3328f7209d41811f
Rick Peterman <rick.peterman@prx.org>
2020-02-14 18:38:50 -0500

    feat(appfooter): layout and svg logos

--- components/AppFooter/AppFooter.styles.ts
+++ components/AppFooter/AppFooter.styles.ts
@@ -7,7 +7,48 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 const appFooterStyles = makeStyles((theme: Theme) => createStyles({
   root: {
-    borderTop: `1px solid ${theme.palette.divider}`
+    marginTop: theme.typography.pxToRem(theme.spacing(3)),
+  },
+  container: {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridRowGap: theme.typography.pxToRem(theme.spacing(2)),
+    justifyItems: 'center',
+
+    padding: theme.typography.pxToRem(theme.spacing(4)),
+
+    '& p': {
+      margin: 0
+    }
+  },
+  twLogo: {
+    width: '30vw',
+    maxWidth: theme.typography.pxToRem(300),
+    height: 'auto',
+    fill: theme.palette.grey['A200']
+  },
+  logos: {
+    display: 'grid',
+    gridTemplateColumns: `repeat(3, max-content)`,
+    gridTemplateRows: `min-content ${theme.typography.pxToRem(25)}`,
+    gridAutoRows: theme.typography.pxToRem(25),
+    gridColumnGap: theme.typography.pxToRem(25),
+    gridRowGap: theme.typography.pxToRem(theme.spacing(1.5)),
+    justifyContent: 'center',
+    justifyItems: 'center',
+    width: '100%'
+  },
+  logosTitle: {
+    gridColumn: '1 / -1',
+    fontStyle: 'italic',
+
+    '&::after': {
+      content: ':'
+    }
+  },
+  logo: {
+    width: 'auto',
+    height: '100%'
   }
 }));
 
--- components/AppFooter/AppFooter.tsx
+++ components/AppFooter/AppFooter.tsx
@@ -4,13 +4,21 @@
  */
 
 import React, { useContext } from 'react';
+import classNames from 'classnames/bind';
 // Material Components
 import {
-  Box
+  Box,
+  Container,
+  Divider
 } from '@material-ui/core';
 import { appFooterStyles } from './AppFooter.styles';
 // Contexts
 import {default as TwAppContext} from '@contexts/AppContext';
+// SVG
+import TwLogo from '@svg/tw-white.svg';
+import PrxLogo from '@svg/PRX-Logo-Horizontal-Color.svg';
+import BBCLogo from '@svg/BBC.svg';
+import WGBHLogo from '@svg/WGBH-Logo.svg';
 
 export default () => {
   const {
@@ -19,8 +27,21 @@ export default () => {
     }
   } = useContext(TwAppContext);
   const classes = appFooterStyles({});
+  const cx = classNames.bind(classes);
 
   return (
-    <Box className={classes.root} height={350} mt={3} />
+    <footer className={classes.root}>
+      <Divider />
+      <Container className={classes.container}>
+        <TwLogo className={classes.twLogo} title="The World" />
+        <p>The World is a public radio program that crosses borders and time zones to bring home the stories that matter.</p>
+        <Box className={cx({ logos: true, producedBy: true})}>
+          <p className={classes.logosTitle}>Produced by</p>
+          <PrxLogo className={classes.logo} />
+          <BBCLogo className={classes.logo} />
+          <WGBHLogo className={classes.logo} />
+        </Box>
+      </Container>
+    </footer>
   );
 };
