ca1e5eb0a6bc68fee31023304f34527bd5dc5bf6
Rick Peterman <rick.peterman@prx.org>
2020-05-14 12:52:21 -0400

    tests: refactor for jsx in imports
    - consolidate all jest config in jest.config.js
    - ts should allow React as unused global

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   roots: ['<rootDir>'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
-    '\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json'
+    }
+  }
 };
