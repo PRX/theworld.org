053bdf95e724b509ea59b3bd63063ab9055fa890
Rick Peterman <rick.peterman@prx.org>
2020-06-18 16:30:52 -0400

    feat(fetchapi): add base function

--- lib/fetch/api/fetchApi.ts
+++ lib/fetch/api/fetchApi.ts
@@ -8,10 +8,30 @@ import { IPriApiResource } from 'pri-api-library/types';
 import { IncomingMessage } from 'http';
 
 /**
+ * Method that simplifies GET requests.
+ *
+ * @param path
+ *    Path to the resource being requested.
+ * @param req
+ *    Request object from `getInitialProps` ctx object.
+ *
+ * @returns
+ *    Denormalized response to request, or error object.
+ */
+export const fetchApi = async (path: string, req: IncomingMessage) => {
+  const baseUrl = req
+    ? `${req.headers['x-forwarded-proto']}://${req.headers.host}`
+    : '';
+  return fetch(`${baseUrl}/api/${path}`).then(resp => resp.json());
+};
+
+/**
  * Metho that simplifies GET queries for resource item using URL path alias.
  *
  * @param alias
  *    Alias used by resource item to display data.
+ * @param req
+ *    Request object from `getInitialProps` ctx object.
  *
  * @returns
  *    Denormalized resource item.
@@ -19,15 +39,4 @@ import { IncomingMessage } from 'http';
 export const fetchApiQueryAlias = async (
   alias: string,
   req: IncomingMessage
-): Promise<IPriApiResource> => {
-  const baseUrl = req
-    ? `${req.headers['x-forwarded-proto']}://${req.headers.host}`
-    : '';
-
-  if (req) {
-    console.log(req.headers);
-    console.log(process.env);
-  }
-
-  return fetch(`${baseUrl}/api/query/alias${alias}`).then(resp => resp.json());
-};
+): Promise<IPriApiResource> => fetchApi(`query/alias${alias}`, req);
