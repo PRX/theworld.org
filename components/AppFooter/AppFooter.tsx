649814ece6a39a434ff7c3148b82c33c30afae18
Brandon Hundt <brandon.hundt@prx.org>
2020-07-23 10:00:42 -0500

    fix(footer): add 501c3 info

--- components/AppFooter/AppFooter.tsx
+++ components/AppFooter/AppFooter.tsx
@@ -121,7 +121,10 @@ export const AppFooter = () => {
           </Breadcrumbs>
         )}
         <p className={classes.copyright}>
-          &copy;{copyrightDate} The World from PRX
+          &copy;{copyrightDate} The World from PRX.
+        </p>
+        <p className={classes.copyright}>
+          PRX is a 501(c)(3) organization recognized by the IRS: #263347402.
         </p>
       </Container>
     </footer>
