c11fe52e0ada2f44b78c51941e0f612568818b25
Rick Peterman <rick.peterman@prx.org>
2021-01-13 17:58:55 -0600

    refactor(program): remove console cmds

--- components/Program/Program.tsx
+++ components/Program/Program.tsx
@@ -166,8 +166,6 @@ export const Program = () => {
     });
   };
 
-  console.log('Program >> ', nextPageUrl, loadMoreUrl, loadMoreAs, loadedPage);
-
   const mainElements = [
     {
       key: 'main top',
