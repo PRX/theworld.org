6a600e0b3737e3006508c240194ca52a356be992
Rick Peterman <rick.peterman@prx.org>
2021-02-24 17:21:10 -0600

    feat(sidebaraudiolist): add data items to store

--- components/Sidebar/SidebarAudioList/SidebarAudioList.tsx
+++ components/Sidebar/SidebarAudioList/SidebarAudioList.tsx
@@ -3,10 +3,12 @@
  * Component for audio list in sidebar items.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStore } from 'react-redux';
 import { ListProps } from '@material-ui/core';
 import { IPriApiResource } from 'pri-api-library/types';
 import { SidebarList } from '../SidebarList';
+import { fetchStoryData } from '@store/actions';
 
 interface ISidebarAudioListProps extends ListProps {
   data: IPriApiResource[];
@@ -16,6 +18,7 @@ export const SidebarAudioList = ({
   data,
   ...other
 }: ISidebarAudioListProps) => {
+  const { dispatch } = useStore();
   const formatTitle = ({ audioTitle: at, title: t }: IPriApiResource) =>
     at || t;
   const linksList =
@@ -23,6 +26,24 @@ export const SidebarAudioList = ({
     data.map((item: IPriApiResource) =>
       item.usage && item.usage.story ? item.usage.story[0] : item
     );
+
+  useEffect(() => {
+    linksList.forEach((item: IPriApiResource) => {
+      const { type, id } = item;
+
+      switch (type) {
+        case 'node--stories':
+          dispatch<any>(fetchStoryData(id as string));
+          break;
+        default:
+          dispatch({
+            type: 'FETCH_CONTENT_DATA_SUCCESS',
+            payload: item
+          });
+          break;
+      }
+    });
+  }, []);
 
   return (
     linksList && (
