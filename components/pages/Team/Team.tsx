1b483a5c32e03220a76d0e352438e9f39a94222e
Rick Peterman <rick.peterman@prx.org>
2021-06-03 16:04:01 -0500

    feat(team): update loading to use router

--- components/pages/Team/Team.tsx
+++ components/pages/Team/Team.tsx
@@ -3,7 +3,8 @@
  * Component for Team.
  */
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { AnyAction } from 'redux';
 import { useStore } from 'react-redux';
@@ -24,8 +25,9 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import { Image } from '@components/Image';
 import { ContentLink } from '@components/ContentLink';
 import { AppContext } from '@contexts/AppContext';
+import { generateLinkHrefForContent } from '@lib/routing';
 import { fetchTeamData } from '@store/actions';
-import { getCollectionData, getLoading } from '@store/reducers';
+import { getCollectionData } from '@store/reducers';
 import { teamStyles, teamTheme } from './Team.styles';
 import { TeamHeader } from './components/TeamHeader';
 
@@ -35,15 +37,33 @@ export const Team = () => {
       resource: { type, id }
     }
   } = useContext(AppContext);
+  const router = useRouter();
+  const [loadingUrl, setLoadingUrl] = useState(null);
   const store = useStore();
   const state = store.getState();
   const classes = teamStyles({});
   const cx = classNames.bind(classes);
   const title = "The World's Team";
   const { items } = getCollectionData(state, type, id, 'members');
-  const loading = getLoading(state);
-  const isLoading = (item: IPriApiResource) =>
-    item.id === loading.id && item.type === loading.type;
+
+  useEffect(() => {
+    const handleRouteChangeStart = (url: string) => {
+      setLoadingUrl(url);
+    };
+    const handleRouteChangeEnd = () => {
+      setLoadingUrl(null);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeEnd);
+    router.events.on('routeChangeError', handleRouteChangeEnd);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeEnd);
+      router.events.off('routeChangeError', handleRouteChangeEnd);
+    };
+  }, []);
 
   return (
     <ThemeProvider theme={teamTheme}>
@@ -56,44 +76,49 @@ export const Team = () => {
         <Grid container spacing={3}>
           {items
             .reduce((a, p) => [...a, ...p], [])
-            .map((item: IPriApiResource) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} key={item.id}>
-                <Card
-                  className={cx({
-                    [classes.isLoading]: isLoading(item) || false
-                  })}
-                >
-                  <CardActionArea>
-                    {item.image && (
-                      <CardMedia>
-                        <Image
-                          data={item.image}
-                          width={{
-                            xl: 290,
-                            lg: 290,
-                            md: 288,
-                            sm: 264,
-                            xs: '100vw'
-                          }}
-                          wrapperClassName={classes.imageWrapper}
+            .map((item: IPriApiResource) => {
+              const { pathname } = generateLinkHrefForContent(item);
+              const isLoading = loadingUrl === pathname;
+
+              return (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={item.id}>
+                  <Card
+                    className={cx({
+                      [classes.isLoading]: isLoading
+                    })}
+                  >
+                    <CardActionArea>
+                      {item.image && (
+                        <CardMedia>
+                          <Image
+                            data={item.image}
+                            width={{
+                              xl: 290,
+                              lg: 290,
+                              md: 288,
+                              sm: 264,
+                              xs: '100vw'
+                            }}
+                            wrapperClassName={classes.imageWrapper}
+                          />
+                        </CardMedia>
+                      )}
+                      <CardContent>
+                        <LinearProgress
+                          className={classes.loadingBar}
+                          color="secondary"
                         />
-                      </CardMedia>
-                    )}
-                    <CardContent>
-                      <LinearProgress
-                        className={classes.loadingBar}
-                        color="secondary"
-                      />
-                      <Typography variant="h4">{item.title}</Typography>
-                      <Typography variant="subtitle1">
-                        {item.position}
-                      </Typography>
-                    </CardContent>
-                    <ContentLink data={item} className={classes.link} />
-                  </CardActionArea>
-                </Card>
-              </Grid>
-            ))}
+                        <Typography variant="h4">{item.title}</Typography>
+                        <Typography variant="subtitle1">
+                          {item.position}
+                        </Typography>
+                      </CardContent>
+                      <ContentLink data={item} className={classes.link} />
+                    </CardActionArea>
+                  </Card>
+                </Grid>
+              );
+            })}
         </Grid>
       </Container>
     </ThemeProvider>
