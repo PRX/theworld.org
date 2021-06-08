8b0664fbb6f5f63cbef96025149755dc4677a76f
Rick Peterman <rick.peterman@prx.org>
2021-06-08 16:51:10 -0500

    refactor(homepage): remove use of comp map

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,36 +4,23 @@
  */
 
 import React from 'react';
-import { IContentComponentProxyProps } from '@interfaces/content';
-import { importComponent, preloadComponent } from '@lib/import/component';
+import { Homepage } from '@components/pages/Homepage';
 import { wrapper } from '@store';
 import { fetchAppData } from '@store/actions';
 
-const resourceType: string = 'homepage';
-
-type Props = IContentComponentProxyProps;
-
-const IndexPage = ({ type }: Props) => {
-  const ContentComponent = importComponent(type);
-
-  return <ContentComponent />;
+const IndexPage = () => {
+  return <Homepage />;
 };
 
 export const getStaticProps = wrapper.getStaticProps(store => async () => {
-  const ContentComponent = await preloadComponent(resourceType);
+  await Promise.all([
+    // Fetch App data (latest stories, menus, etc.)
+    store.dispatch<any>(fetchAppData()),
+    // Use content component to fetch its data.
+    store.dispatch<any>(Homepage.fetchData())
+  ]);
 
-  if (ContentComponent) {
-    await Promise.all([
-      // Fetch App data (latest stories, menus, etc.)
-      store.dispatch<any>(fetchAppData()),
-      // Use content component to fetch its data.
-      store.dispatch(ContentComponent.fetchData())
-    ]);
-
-    return { props: { type: resourceType }, revalidate: 10 };
-  }
-
-  return { notFound: true };
+  return { props: {}, revalidate: 10 };
 });
 
 export default IndexPage;
