473c38d2a505de7acff3b0232a21a7ee9b181729
Rick Peterman <rick.peterman@prx.org>
2021-10-19 16:07:09 -0500

    fix(newsletter): prevent form overflow

--- components/pages/Newsletter/Newsletter.styles.ts
+++ components/pages/Newsletter/Newsletter.styles.ts
@@ -31,7 +31,9 @@ export const newsletterStyles = makeStyles((theme: Theme) =>
         gridTemplateRows: '2fr max-content 2rem',
         gridGap: '1rem',
         justifyItems: 'center',
+        alignContent: 'end',
         overflow: 'hidden',
+        minHeight: '75vh',
         marginBottom: `-${theme.spacing(3)}px`,
         '& > *': {
           position: 'relative',
@@ -56,9 +58,6 @@ export const newsletterStyles = makeStyles((theme: Theme) =>
         },
         '&::after': {
           transform: 'scale(2) rotate(6deg) translateY(10%)'
-        },
-        [theme.breakpoints.up('sm')]: {
-          maxHeight: '75vh'
         }
       }
     },
@@ -66,10 +65,7 @@ export const newsletterStyles = makeStyles((theme: Theme) =>
     imageWrapper: {
       gridColumn: '1 / -1',
       gridRow: '1 / -1',
-      justifySelf: 'stretch',
-      [theme.breakpoints.up('sm')]: {
-        maxHeight: '75vh'
-      }
+      justifySelf: 'stretch'
     },
     content: {
       '$withImage &': {
