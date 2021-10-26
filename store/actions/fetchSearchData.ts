0df0cb8010bf224c698fef5c530f33190a28737a
Rick Peterman <rick.peterman@prx.org>
2021-10-26 17:10:34 -0500

    fix(fetchsearchdata): require fetch GCS relative

--- store/actions/fetchSearchData.ts
+++ store/actions/fetchSearchData.ts
@@ -64,7 +64,7 @@ export const fetchSearchData = (
     if (req) {
       // eslint-disable-next-line no-eval
       const fetchGoogleCustomSearch = eval(
-        "require('@lib/fetch/search/fetchGoogleCustomSearch')"
+        "require('../../lib/fetch/search/fetchGoogleCustomSearch')"
       );
       results = fetchGoogleCustomSearch(query, l, start);
     } else {
