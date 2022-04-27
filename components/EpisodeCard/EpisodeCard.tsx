910530a519b2260bb5cf6745df3539eb63385e9b
brandonhundt <brandonhundt@gmail.com>
2022-04-27 13:59:37 -0500

    feat(episodeCard): teaser is back

--- components/EpisodeCard/EpisodeCard.tsx
+++ components/EpisodeCard/EpisodeCard.tsx
@@ -10,14 +10,17 @@ import Image from 'next/image';
 import classNames from 'classnames/bind';
 import { IPriApiResource } from 'pri-api-library/types';
 import {
+  Box,
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
   Typography
 } from '@material-ui/core';
+import { EqualizerRounded } from '@material-ui/icons';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { ContentLink } from '@components/ContentLink';
+import { HtmlContent } from '@components/HtmlContent';
 import { SidebarAudioList } from '@components/Sidebar/SidebarAudioList';
 import { episodeCardStyles, episodeCardTheme } from './EpisodeCard.styles';
 
@@ -30,7 +33,7 @@ export interface EpisodeCardProps {
 }
 
 export const EpisodeCard = ({ data, priority }: EpisodeCardProps) => {
-  const { title, image, audio, dateBroadcast, datePublished } = data;
+  const { title, teaser, image, audio, dateBroadcast, datePublished } = data;
   const { segments } = audio || {};
   const classes = episodeCardStyles({});
   const cx = classNames.bind(classes);
@@ -71,9 +74,19 @@ export const EpisodeCard = ({ data, priority }: EpisodeCardProps) => {
             >
               {title}
             </Typography>
+            <Typography variant="body1" component="div" color="textSecondary">
+              <Box className={cx('body')}>
+                <HtmlContent html={teaser} />
+              </Box>
+            </Typography>
             <ContentLink data={data} className={cx('link')} />
             {segments && (
               <>
+                <Box component="header" className={cx('header')}>
+                  <Typography variant="h2">
+                    <EqualizerRounded /> In this episode:
+                  </Typography>
+                </Box>
                 <SidebarAudioList data={segments} />
               </>
             )}
