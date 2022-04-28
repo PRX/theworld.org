d1cd00c0f6139376de153ef63d99d74940f5f48c
Rick Peterman <rick.peterman@prx.org>
2022-04-28 11:29:00 -0500

    feat(quicklinks): fetch data w/ homepage data

--- pages/api/homepage.ts
+++ pages/api/homepage.ts
@@ -5,26 +5,33 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { IPriApiCollectionResponse } from 'pri-api-library/types';
 import { basicStoryParams } from '@lib/fetch/api/params';
-import { fetchApiProgram, fetchPriApiQuery } from '@lib/fetch';
+import {
+  fetchApiProgram,
+  fetchPriApiQuery,
+  fetchPriApiQueryMenu
+} from '@lib/fetch';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // Program Data
-  const program = await fetchApiProgram('3704', req);
-
-  // Latest Non-TW stories
-  const latestStories = (await fetchPriApiQuery('node--stories', {
-    ...basicStoryParams,
-    'filter[status]': 1,
-    'filter[program][value]': 3704,
-    'filter[program][operator]': '<>',
-    sort: '-date_published',
-    range: 10
-  })) as IPriApiCollectionResponse;
+  const [program, latestStories, quickLinks] = await Promise.all([
+    fetchApiProgram('3704').then(resp => resp && resp.data),
+    fetchPriApiQuery('node--stories', {
+      ...basicStoryParams,
+      'filter[status]': 1,
+      'filter[program][value]': 3704,
+      'filter[program][operator]': '<>',
+      sort: '-date_published',
+      range: 10
+    }).then((resp: IPriApiCollectionResponse) => resp),
+    fetchPriApiQueryMenu('menu-the-world-quick-links')
+  ]);
 
   const apiResp = {
     data: {
       ...program,
-      latestStories
+      latestStories,
+      menus: {
+        quickLinks
+      }
     }
   };
 
