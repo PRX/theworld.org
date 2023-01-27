6f0e0f03d5e7ac777f196efcc415b705e78d2e3a
Rick Peterman <rick.peterman@prx.org>
2023-01-27 11:48:51 -0600

    fix(playaudiobutton): primary color when playing

--- components/Player/components/PlayAudioButton/PlayAudioButton.styles.ts
+++ components/Player/components/PlayAudioButton/PlayAudioButton.styles.ts
@@ -46,10 +46,10 @@ export const playAudioButtonStyles = makeStyles<{ audioIsPlaying: boolean }>()(
         }
       }),
       ...(audioIsPlaying && {
-        backgroundColor: theme.palette.secondary.main,
-        color: theme.palette.secondary.contrastText,
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.primary.contrastText,
         '&:hover': {
-          backgroundColor: theme.palette.secondary.light
+          backgroundColor: theme.palette.primary.light
         }
       })
     },
