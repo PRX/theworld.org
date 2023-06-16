16697fe92331ba54a3c8389fc851e6271a3c155e
Rick Peterman <rick.peterman@prx.org>
2023-06-16 09:58:53 -0500

    fix(fetchappdata): type errors

--- store/actions/fetchAppData.ts
+++ store/actions/fetchAppData.ts
@@ -8,43 +8,50 @@ import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { RootState } from '@interfaces/state';
 import { fetchApiApp, fetchApp } from '@lib/fetch';
 import { getCollectionData } from '@store/reducers';
-import { appendResourceCollection } from './appendResourceCollection';
-import { fetchCtaRegionGroupData } from './fetchCtaRegionGroupData';
+// import { appendResourceCollection } from './appendResourceCollection';
+// import { fetchCtaRegionGroupData } from './fetchCtaRegionGroupData';
 
-export const fetchAppData = (): ThunkAction<void, {}, {}, AnyAction> => async (
-  dispatch: ThunkDispatch<{}, {}, AnyAction>,
-  getState: () => RootState
-): Promise<void> => {
-  const state = getState();
-  const isOnServer = typeof window === 'undefined';
-  const latest = getCollectionData(state, 'app', undefined, 'latest');
+export const fetchAppData =
+  (): ThunkAction<void, {}, {}, AnyAction> =>
+  async (
+    dispatch: ThunkDispatch<{}, {}, AnyAction>,
+    getState: () => RootState
+  ) => {
+    const state = getState();
+    const isOnServer = typeof window === 'undefined';
+    const latest = getCollectionData(state, 'app', undefined, 'latest');
 
-  if (!latest || isOnServer) {
-    dispatch({
-      type: 'FETCH_APP_DATA_REQUEST'
-    });
+    if (!latest || isOnServer) {
+      dispatch({
+        type: 'FETCH_APP_DATA_REQUEST'
+      });
 
-    const apiRespPromise = (isOnServer ? fetchApp : fetchApiApp)();
-    const ctaDataPromise = dispatch<any>(
-      fetchCtaRegionGroupData('tw_cta_regions_site')
-    );
+      const apiRespPromise = (isOnServer ? fetchApp : fetchApiApp)();
+      // const ctaDataPromise = dispatch<any>(
+      //   fetchCtaRegionGroupData('tw_cta_regions_site')
+      // );
 
-    const apiResp = await apiRespPromise;
-    await ctaDataPromise;
+      const apiResp = await apiRespPromise;
 
-    const { latestStories, menus } = apiResp;
+      if (apiResp) return apiResp;
 
-    dispatch(
-      appendResourceCollection(latestStories, 'app', undefined, 'latest')
-    );
+      // await ctaDataPromise;
 
-    dispatch({
-      type: 'FETCH_MENUS_DATA_SUCCESS',
-      payload: menus
-    });
+      // const { latestStories, menus } = apiResp || {};
 
-    dispatch({
-      type: 'FETCH_APP_DATA_SUCCESS'
-    });
-  }
-};
+      // dispatch(
+      //   appendResourceCollection(latestStories, 'app', undefined, 'latest')
+      // );
+
+      // dispatch({
+      //   type: 'FETCH_MENUS_DATA_SUCCESS',
+      //   payload: menus
+      // });
+
+      // dispatch({
+      //   type: 'FETCH_APP_DATA_SUCCESS'
+      // });
+    }
+
+    return undefined;
+  };
